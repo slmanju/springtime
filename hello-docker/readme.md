1d71dff974c8ee080e9df3720711530b3bf072d9
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-08-30 17:08:27 +0530

    add docker support

---
+++ hello-docker/readme.md
@@ -0,0 +1,6 @@
+## Say Hello to Docker
+
+### Build
+`gradle clean build`  
+`docker build -f Dockerfile -t hello-docker .`  
+`docker run -p 8085:8085 hello-docker`
---
+++ hello-docker/src/main/java/com/manjula/docker/HelloDockerApplication.java
@@ -0,0 +1,12 @@
+package com.manjula.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerApplication.class, args);
+	}
+}
